b3a5759c7fab4143ddecab3b2a21de51c8902abe
Seyma Gilbaz <seymagilbaz@gmail.com>
2019-12-29 09:02:34 +0300

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Şeyma Gılbaz on 29.12.2019.
+//  Copyright © 2019 Şeyma Gılbaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
